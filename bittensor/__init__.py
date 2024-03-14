b3d60f4b2a645f8c66cc6b0a063c9789222e98d1
Sepehr Ghavam <robotlove@Sepehrs-MacBook-Pro.local>
2024-03-14 17:14:24 -0400

    Remove loguru from btlogging.
    To enable and manage third party loggers, loguru needed to be replaced with the builtin logging library. To maintain functional parity and best manage the logging infra state, the new log manager was built using a state machine, in order to explicitly handle transitions between the various logger states.

--- bittensor/__init__.py
+++ bittensor/__init__.py
@@ -218,7 +218,8 @@ from .utils.balance import Balance as Balance
 from .chain_data import *
 from .subtensor import subtensor as subtensor
 from .cli import cli as cli, COMMANDS as ALL_COMMANDS
-from .btlogging import logging as logging
+from .btlogging import logging
+# from .logging import LoggingMachine, 
 from .metagraph import metagraph as metagraph
 from .threadpool import PriorityThreadPoolExecutor as PriorityThreadPoolExecutor
 
@@ -239,6 +240,6 @@ configs = [
     subtensor.config(),
     PriorityThreadPoolExecutor.config(),
     wallet.config(),
-    logging.config(),
+    logging.config,
 ]
 defaults = config.merge_all(configs)
